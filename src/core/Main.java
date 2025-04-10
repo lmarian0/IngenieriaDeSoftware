d13196c1f1bf9888705359f58dc6ac7cc93ca61f
lmarian0 <luismarianorivera.25@mi.unc.edu.ar>
2025-04-09 21:31:51 -0300

    Update 'src/map' and 'src/core' from feature/thiago

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,17 +1,28 @@
 package core;
-import map.Map;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import map.GameMap;
 import character.Player;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Arrancando");
 
-        Map mapa = new Map(200, 100);
+public class Main extends Application {
 
-        System.out.println(mapa);
-        mapa.getMapMeasures();
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Cargando imagen...");
 
-        Player p1 = new Player("Osvaldo", 100, 1, 10, 1.2f, 2f, 0, 0, 0);
+        // Cargá tu imagen (ruta absoluta o desde resources)
+        Image image = new Image("file:src/resources/GOKUMAPAEDO.jpg");
+
+        // Crear el mapa lógico
+        GameMap gameMap = new GameMap(image);
+        Player p1 = new Player("Osvaldo", 100, 5, 10, 0,0,5, 5, 5);
         System.out.println(p1.getDmg());
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Lanza JavaFX
     }
 }
