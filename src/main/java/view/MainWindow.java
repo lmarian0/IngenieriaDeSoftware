f40726ee55aa065ef6bb2313bc8156c68c621e03
Thiago Biancucci <thiago.biancucci@mi.unc.edu.ar>
2025-06-08 20:55:06 -0300

    Update MainWindow.java

--- src/main/java/view/MainWindow.java
+++ src/main/java/view/MainWindow.java
@@ -6,11 +6,11 @@ import java.awt.*;
 
 public class MainWindow extends JFrame {
 
-    public MainWindow(Display display, GraphicsDevice device) {
+    public MainWindow(Display display) {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUndecorated(true); // Quita la barra de título
 
-        device.setFullScreenWindow(this);
+        //device.setFullScreenWindow(this);
 
         add(display);
         setVisible(true);
