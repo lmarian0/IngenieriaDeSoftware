d25ef00b7a6a05b3e3b093dfd28290dc5d71b483
Thiago Biancucci <thiago.biancucci@mi.unc.edu.ar>
2025-06-17 22:33:27 -0300

    Eliminacion de MapConstants
    ScreenSettings cumple su funcion y de forma mas actualizada y eficiente.

--- src/main/java/controller/Camera.java
+++ src/main/java/controller/Camera.java
@@ -1,18 +1,19 @@
 package main.java.controller;
 
 import main.java.model.Player;
-import main.java.model.constants.MapConstants;
+import main.java.model.constants.ScreenSettings;
 
 public class Camera {
 
     private int offsetX;
     private int offsetY;
+    private ScreenSettings screenSettings;
 
     public void update(Player player) {
-        int screenWidth = MapConstants.SCREEN_WIDTH;
-        int screenHeight = MapConstants.SCREEN_HEIGHT;
-        int worldWidth = MapConstants.MAP_WIDTH * MapConstants.TILE_SIZE;
-        int worldHeight = MapConstants.MAP_HEIGHT * MapConstants.TILE_SIZE;
+        int screenWidth = screenSettings.getScaledWidth(screenSettings);
+        int screenHeight = screenSettings.getScaledHeight(screenSettings);
+        int worldWidth = screenSettings.getScreenCols();
+        int worldHeight = screenSettings.getScreenRows();
 
         // Centrar al jugador
         offsetX = player.getPosX() - screenWidth / 2 + player.getWidth() / 2;
