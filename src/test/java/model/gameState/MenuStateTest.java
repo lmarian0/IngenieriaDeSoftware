95703ae1dca5709a694c1ba933334895905e28c4
Thiago Biancucci <thiago.biancucci@mi.unc.edu.ar>
2025-06-19 04:32:08 -0300

    Test for MenuState and PlayingState
    Verifican el correcto ingreso del spacebar para intercambiar con playingState
    y en Playing state el correcto intercambio de estado a gameover cuando el personaje muere

--- src/test/java/model/gameState/MenuStateTest.java
+++ src/test/java/model/gameState/MenuStateTest.java
@@ -1,5 +1,25 @@
 package test.java.model.gameState;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import main.java.controller.Controller;
+import main.java.controller.KeyHandler;
+import main.java.model.Player;
+import main.java.model.gameState.MenuState;
+import main.java.model.gameState.PlayingState;
+
 public class MenuStateTest {
-    
+    @Test
+    public void testSpaceKeyChangesState() {
+        KeyHandler keyHandler = new KeyHandler();
+        Player player = Player.getInstance();
+        Controller controller = new Controller(player, keyHandler);
+        MenuState menuState = new MenuState(keyHandler, controller);
+        controller.setEstadoActual(menuState);
+
+        keyHandler.space = true;
+        menuState.update();
+
+        assertTrue(controller.getEstadoActual() instanceof PlayingState);
+    }
 }
